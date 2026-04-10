8359be4e8b316c9238b349b7ba522fbc47c6246b
Elliyani <ellit3ch@gmail.com>
2026-04-10 13:59:16 +0000

    Improved HTML structure to include my name and LinkedIn profile link

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,14 @@
 from flask import Flask, redirect, render_template, request, url_for
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comments.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
